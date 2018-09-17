84e410f8405c9cd1c9f658f664feb80a2027301b
mre <mre@ableton.com>
2018-09-17 11:23:17 +0200

    Add some comments

--- js/model.js
+++ js/model.js
@@ -1,8 +1,13 @@
 
 Model = function()
 {
+  // current root note
   this.root = 'C';
+  // current scale
   this.scale = 'major';
+  // octave
   this.octave = 4;
+  // Assuming a 4 part harmony, specifies whether a part is active or not
+  // part 0 is the bass
   this.activeVoices = [true, true, true, true];
 }
